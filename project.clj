4531ea3ae08c591d8dd10acd665c46785d38fcf2
Mihael Konjevic <konjevic@gmail.com>
2017-05-05 23:54:33 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mihaelkonjevic/garden-basscss "0.1.0"
+(defproject org.clojars.mihaelkonjevic/garden-basscss "0.2.0"
   :description "Port of BassCSS library to Garden"
   :url "http://github.com/retro/garden-basscss"
   :license {:name "MIT"}
