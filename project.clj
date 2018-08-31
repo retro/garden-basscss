f22a66e07676b983e2c85820db819fed4cc515a7
Tibor Kranjcec <tibor@floatingpoint.io>
2018-08-31 19:45:50 +0200

    Update versions

--- project.clj
+++ project.clj
@@ -5,11 +5,9 @@
 
   :min-lein-version "2.6.1"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51"]
-                 [garden "1.3.2"]
-                 [org.clojure/core.async "0.2.374"
-                  :exclusions [org.clojure/tools.reader]]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.312"]
+                 [garden "1.3.2"]]
 
   :plugins [[lein-figwheel "0.5.4-3"]
             [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
